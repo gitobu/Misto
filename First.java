206d791783c27ac7b0a16a38c5779e1f70041d46
Allan Gitobu <allan.mugambi@cgu.edu>
2014-01-13 21:08:02 -0800

    Update First.java

--- First.java
+++ First.java
@@ -1,2 +1,20 @@
-class Misto{
+public class Misto {
+	private String firstName;
+	private String lastName;
+	private int blocks;
+	
+	public void showName(){
+		System.out.println(firstName + ' ' + lastName + " made " + blocks + " blocks.");
+		
+	}
+	
+	public static void main(String[] args) {
+		Misto hj = new Misto();
+		hj.firstName = "King";
+		hj.lastName = "Dunlap";
+		hj.blocks = 34;
+		hj.showName();
+	
+	}
+
 }
